7b70de4fa12bc6b6f1fcb9a22b780a49337842c7
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2024-02-02 19:14:54 +0000

    IPV6 Support

--- entry.sh
+++ entry.sh
@@ -4,4 +4,4 @@ service clamav-daemon start
 service clamav-freshclam start
 
 # Then start the scanner.py Flask app with Gunicorn
-exec gunicorn -w 4 -b 0.0.0.0:5000 scanner:app
+exec gunicorn -w 4 -b [::]:5000 scanner:app
